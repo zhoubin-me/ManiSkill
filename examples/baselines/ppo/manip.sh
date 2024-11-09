3cd583aae162910592fb5a83bf28ccb8085eda7c
ZHOU Bin <zhoubin.me@gmail.com>
2024-11-10 01:09:25 +0800

    manip.sh

---
+++ examples/baselines/ppo/manip.sh
@@ -0,0 +1,5 @@
+
+
+python ppo.py --track --wandb_project_name manip --total_timesteps 10000000
+python ppo_rgb.py --track --wandb_project_name manip --total_timesteps 20000000
+python ppo_rgbd.py --track --wandb_project_name manip --total_timesteps 20000000
